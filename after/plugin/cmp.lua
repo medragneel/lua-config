362ab1a60fe69288290b607935ae6d04d75b0fcf
mshadow <medmystgon@gmail.com>
2022-10-18 16:13:53 +0100

    v3.5.2

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,7 +1,6 @@
 
 local ok_cmp, cmp = pcall(require, "cmp")
 
-require("luasnip.loaders.from_vscode").lazy_load()
 cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
@@ -48,3 +47,22 @@ require'lspconfig'.cssls.setup {
 }
 
 
+local snippets_paths = function()
+	local plugins = { "friendly-snippets" }
+	local paths = {}
+	local path
+	local root_path = vim.env.HOME .. "/.vim/plugged/"
+	for _, plug in ipairs(plugins) do
+		path = root_path .. plug
+		if vim.fn.isdirectory(path) ~= 0 then
+			table.insert(paths, path)
+		end
+	end
+	return paths
+end
+
+require("luasnip.loaders.from_vscode").lazy_load({
+	paths = snippets_paths(),
+	include = nil, -- Load all languages
+	exclude = {},
+})
